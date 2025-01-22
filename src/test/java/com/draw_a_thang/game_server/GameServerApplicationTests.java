897b14a5702dc21cf9881b4a0d610eb014a91c0f
Dat Ngo <datngonyc@gmail.com>
2025-01-21 23:58:33 -0500

    init commit.

---
+++ src/test/java/com/draw_a_thang/game_server/GameServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.draw_a_thang.game_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
